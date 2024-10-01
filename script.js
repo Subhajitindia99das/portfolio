b27f946f772c71ca712cfa3039a8a85e9b22fba2
Subhajit Das <72573641+Subhajitindia99das@users.noreply.github.com>
2024-10-02 01:39:03 +0530

    Changes are done in this project

---
+++ script.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+    distance: '100px',
+    duration: 1200,
+    delay: 500
+});
+
+ScrollReveal().reveal('.navlist', { origin: "top" });
+ScrollReveal().reveal('.content', { origin: "left" });
+ScrollReveal().reveal('.imga', { origin: "right" });
+ScrollReveal().reveal('.newbox', { origin: "right" });
+ScrollReveal().reveal('form', { origin: "left" });
+ScrollReveal().reveal('.imgs', { origin: "top" });
+// ScrollReveal().reveal('.tagline', { origin: 'bottom' });
